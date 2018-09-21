870be0a709ae2add595329a047de573d11c68e01
ldlqdsdcn <ldlqdsdcn@gmail.com>
2018-09-21 17:00:26 +0800

    带上挖矿命令

--- shell/run.sh
+++ shell/run.sh
@@ -1,2 +1,2 @@
  #启动以太坊命理
- geth --datadir node/ console --rpcapi "admin,personal,db,eth,net,web3,txpool,miner" --rpc --rpcport 8501
+ geth --datadir node/ console --rpcapi "admin,personal,db,eth,net,web3,txpool,miner" --rpc --rpcport 8501 --mine
